8160195b96815c9d03f139e98ba64dabc353e156
Irtaza <khalidmi@cardiff.ac.uk>
2024-01-25 11:35:10 +0000

    fix message string linux

--- notifications.py
+++ notifications.py
@@ -7,11 +7,12 @@ logging.basicConfig(level=logging.INFO)
 
 def eye_rest_notification(mins=20):
     # print "rest" every `min` minutes
-    message = 'Its been 20 min. Look away for 20 seconds.'
+    message = "Its been 20 min. Look away for 20 seconds."
     if 'linux' in sys.platform: 
+        logging.info('booting up in linux 😊/💀')
         while True:
-            os.system(f"notify-send {message}")
-            time.sleep(mins*60) # sleep for 20*60 seconds
+            os.system(f"notify-send '{message}'")
+            # time.sleep(mins*60) # sleep for 20*60 seconds
     elif 'win' in sys.platform:
         try:
             import win10toast
@@ -24,6 +25,7 @@ def eye_rest_notification(mins=20):
         except ModuleNotFoundError as e:
             logging.error("Failed to install win10toast. If this fails, just do it manually using privileged pip.")
             raise e
+        logging.info('booting up in linux 😊/💀')
         toaster = win10toast.ToastNotifier()
         while True:
             toaster.show_toast(title="Rest Notification", msg=message, duration=5)
